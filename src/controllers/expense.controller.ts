3e960b1aba8fa63e60d14a0aa2f7912c2e513d0f
Abdi-01 <abdialghi@gmail.com>
2024-08-09 14:38:41 +0700

    implement redis

--- src/controllers/expense.controller.ts
+++ src/controllers/expense.controller.ts
@@ -1,9 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 import prisma from "../prisma";
+import { redisClient } from "../utils/redisClient";
 
 export class ExpenseController {
   async getDataExpense(req: Request, res: Response, next: NextFunction) {
     try {
+      // Check data in redis
+      const redisData = await redisClient.get("expense-all-byId");
+      console.log("FROM REDIS", redisData);
+
+      if (redisData) {
+        return res.status(200).send({
+          success: true,
+          result: JSON.parse(redisData),
+        });
+      }
+
       const data = await prisma.track.findMany({
         include: {
           user: true,
@@ -12,6 +24,9 @@ export class ExpenseController {
           userId: res.locals.decript.id,
         },
       });
+
+      // Set data to redis
+      await redisClient.setEx("expense-all-byId", 10, JSON.stringify(data));
 
       return res.status(200).send({
         success: true,
